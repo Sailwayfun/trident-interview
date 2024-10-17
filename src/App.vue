644fcf6649776f6b9ffb444a1909cd46c849d987
Sailwayfun <liaoleon000513@gmail.com>
2024-10-17 20:31:54 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1>Trident Interview</h1>
+    </div>
+  </header>
+
+  <main>
+    <div class="wrapper">
+      <h2>Trident Interview</h2>
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
