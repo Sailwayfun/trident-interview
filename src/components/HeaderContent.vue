2cc283961a7e8e1e90575346ebcb5c23943f5f7b
Sailwayfun <liaoleon000513@gmail.com>
2024-10-18 21:55:55 +0800

    logo...pure css bird

--- src/components/HeaderContent.vue
+++ src/components/HeaderContent.vue
@@ -13,7 +13,12 @@
       </div>
     </nav>
     <div>
-      <div class="logo" :class="{ open: isOpen }"></div>
+      <div class="logo" :class="{ open: isOpen }">
+        <div class="bird">
+          <div class="eye"></div>
+          <div class="beak"></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -79,6 +84,41 @@ a.active {
     transform: translateY(-10px);
   }
 }
+
+.bird {
+  position: relative;
+  left: 10px;
+  top: 10px;
+  width: 25px;
+  height: 25px;
+  background-color: #f5f5f5;
+  border-radius: 150% 100% 100% 0;
+  border-top: 4px solid #333;
+  border-left: none;
+  border-bottom: 3px solid #333;
+}
+
+.eye {
+  position: absolute;
+  top: 7px;
+  left: 12px;
+  width: 6px;
+  height: 6px;
+  background-color: #333;
+  border-radius: 50%;
+}
+
+.beak {
+  position: absolute;
+  top: 3px;
+  left: 25px;
+  width: 8px;
+  height: 4px;
+  background-color: #333;
+  border-radius: 0 450% 0% 0;
+  transform: rotate(-20deg);
+}
+
 @media only screen and (min-width: 1280px) {
   .header-content {
     align-items: flex-start;
@@ -105,6 +145,39 @@ a.active {
     transform: translateX(-50%);
     z-index: 5;
     top: 3rem;
+  }
+  .bird {
+    position: relative;
+    left: 1.5rem;
+    top: 1.5rem;
+    width: 50px;
+    height: 45px;
+    background-color: #f5f5f5;
+    border-radius: 150% 100% 100% 0;
+    border-top: 8px solid #333;
+    border-left: none;
+    border-bottom: 6px solid #333;
+  }
+
+  .eye {
+    position: absolute;
+    top: 12px;
+    left: 30px;
+    width: 8px;
+    height: 8px;
+    background-color: #333;
+    border-radius: 50%;
+  }
+
+  .beak {
+    position: absolute;
+    top: 8px;
+    left: 50px;
+    width: 8px;
+    height: 6px;
+    background-color: #333;
+    border-radius: 0 150% 0% 0;
+    transform: rotate(-10deg);
   }
 }
 </style>
